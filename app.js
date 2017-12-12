7b9aaf41942b9d550bf05cabe9b9cec42d75799e
fzbo <elizabeth.bazan@gmail.com>
2017-12-12 12:54:38 -0500

    Update links and script references added

---
+++ app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC28Wa9GxixBTwu6gBEssFwZTrdXRAoot0",
+    authDomain: "traintime-6b899.firebaseapp.com",
+    databaseURL: "https://traintime-6b899.firebaseio.com",
+    projectId: "traintime-6b899",
+    storageBucket: "",
+    messagingSenderId: "345205755438"
+  };
+  firebase.initializeApp(config);
